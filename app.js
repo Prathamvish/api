7c02b1db28d975493c0e6a39bc3b6790f300f50a
Pratham Vishwakarma <you@example.com>
2022-04-15 00:55:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.json({ limit: '10kb'}));
+
+app.post('/bfhl', (req, res, next) => {
+    let array = req.body.array;
+
+    array.forEach(element => {
+        if(isNaN(element)){
+            return res.status(422).json({
+                is_succcess: true,
+                user_id: "pratham_vishwakarma_26012000",
+            })
+        }
+    });
+
+    let odd = [];
+    let even = [];
+    console.log(array);
+    array.forEach(element => {
+        if((element*1)%2 == 0){
+            even.push(element*1);
+        }else{
+            odd.push(element*1);
+        }
+    })
+
+    res.status(200).json({
+        is_succcess: true,
+        user_id: "pratham_vishwakarma_26012000",
+        odd_numbers: odd,
+        even_numbers: even
+    })
+})
+
+app.listen(4000, () => {
+    console.log('Server Started on Port 4000');
+})
